eda9d3ef6d5f663d57109e4c292cb14102ce0d5b
Ashutosh-Negi-Creator <bhimsinghnegi178@gmail.com>
2025-09-23 23:25:12 +0530

    Create Ques8.cpp

---
+++ Assignment_3_(23_Sep)/Ques8.cpp
@@ -0,0 +1,16 @@
+// FIBONACCI NUMBR AT N BY RECURSION
+
+#include<iostream>
+using namespace std;
+
+int fibo(int n){
+    if(n==2 || n==1) return 1;
+    return fibo(n-1)+fibo(n-2);
+}
+int main(){
+    int n;
+    cout<<"Enter the number of months :-"<<endl;
+    cin>>n;
+    cout<<fibo(n);
+    return 0;
+}

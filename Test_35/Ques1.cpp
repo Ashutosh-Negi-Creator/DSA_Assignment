ba31290e18690288a6ad74e07463040bd1894665
Ashutosh-Negi-Creator <bhimsinghnegi178@gmail.com>
2025-10-07 20:39:34 +0530

    Update Ques1.cpp

--- Test_35/Ques1.cpp
+++ Test_35/Ques1.cpp
@@ -5,25 +5,24 @@ Input: n=5, arr=[2,7,1,9,5]
 Output: 9
 Constraints: 1 ≤ n ≤ 10^5, -10^9 ≤ arr[i] ≤ 10^9
 */
-
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<iostream>
 using namespace std;
 
 int main() {
     int n;
-    cout << "Enter number of plates: ";
     cin >> n;
-    
-    vector<int> arr(n);
-    cout << "Enter quantities: ";
+    int arr[100005];
     for(int i = 0; i < n; i++) {
         cin >> arr[i];
     }
     
-    int maxFood = *max_element(arr.begin(), arr.end());
-    cout << "Maximum food plate: " << maxFood << endl;
+    int maxFood = arr[0];
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > maxFood) {
+            maxFood = arr[i];
+        }
+    }
     
+    cout << maxFood << endl;
     return 0;
 }

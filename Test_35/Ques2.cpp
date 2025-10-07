0362f58c308bef6d1c5a1bdffd50e1f6f6fea6f9
Ashutosh-Negi-Creator <bhimsinghnegi178@gmail.com>
2025-10-07 20:40:21 +0530

    Update Ques2.cpp

--- Test_35/Ques2.cpp
+++ Test_35/Ques2.cpp
@@ -5,32 +5,24 @@ Input: n=5, arr=[0,1,2,4,5]
 Output: 3
 Constraints: O(n) or O(log n) solution required.
 */
-
-#include <iostream>
-#include <vector>
+#include<iostream>
 using namespace std;
 
 int main() {
     int n;
-    cout << "Enter n: ";
     cin >> n;
-    
-    vector<int> arr(n);
-    cout << "Enter soldiers: ";
+    int arr[100005];
     for(int i = 0; i < n; i++) {
         cin >> arr[i];
     }
     
-    // Using sum formula: sum of 0 to n = n*(n+1)/2
     int expectedSum = n * (n + 1) / 2;
     int actualSum = 0;
-    
     for(int i = 0; i < n; i++) {
         actualSum += arr[i];
     }
     
     int missing = expectedSum - actualSum;
-    cout << "Missing soldier: " << missing << endl;
-    
+    cout << missing << endl;
     return 0;
 }
